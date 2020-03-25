efe320b7b234a7b680be310eef0d926ea4e620e2
Kale Franz <kfranz@continuum.io>
2020-03-25 17:46:48 -0500

    fix order
    Signed-off-by: Kale Franz <kfranz@continuum.io>

--- generate-index.py
+++ generate-index.py
@@ -93,8 +93,8 @@ _TEMPLATE = """\
 """
 
 files = (
-    "repodata-record-1.schema.json",
     "repodata-1.schema.json",
+    "repodata-record-1.schema.json",
 )
 
 rendered = Template(_TEMPLATE).render(files=files)
