a68c6a3a4802100f281de363c8103c55242b281a
jaimergp <jaimergp@users.noreply.github.com>
2023-05-09 13:38:44 +0200

    add environment.yml
    Co-authored-by: AlbertDeFusco <AlbertDeFusco@users.noreply.github.com>

--- conda_models/environment_file.py
+++ conda_models/environment_file.py
@@ -1,3 +1,35 @@
 """
 WIP
 """
+from typing import Dict, Iterable, Union
+
+from pydantic import DirectoryPath, validator
+
+from ._base import ExtrasForbiddenModel
+from .types import MatchSpecStr, NonEmptyStr, PackageNameStr, SubdirStr
+
+
+class EnvironmentYaml(ExtrasForbiddenModel):
+    name: str = None
+    "Name for the environment"
+    channels: Iterable[NonEmptyStr] = None
+    "Channels to search for packages"
+    dependencies: Union[
+        str,
+        Iterable[Union[MatchSpecStr, Dict[PackageNameStr, Iterable[str]]]],
+    ] = ()
+    "Packages to install into the environment, as a series of match specifications."
+    variables: Dict[NonEmptyStr, str] = None
+    "Shell variables to define for the environment."
+    prefix: DirectoryPath = None
+    "Path where the environment should be created."
+    platforms: Iterable[SubdirStr] = None
+    "Platforms to search for packages"
+
+    @validator("dependencies")
+    def only_pip_key_allowed(cls, v):
+        for item in v:
+            if isinstance(item, dict) and item.keys() != {"pip"}:
+                msg = "Only 'pip: [str]' is allowed in 'dependencies'"
+                raise ValueError(msg)
+        return v
