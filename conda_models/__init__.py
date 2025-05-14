401aee563dd55f141ccff3e4ab07b18729d3b941
jaimergp <jaimergp@users.noreply.github.com>
2025-05-14 11:01:29 +0200

    Add repodata.info.json

--- conda_models/__init__.py
+++ conda_models/__init__.py
@@ -7,6 +7,7 @@ from .meta_yaml import MetaYaml
 from .package_info import InfoAbout, InfoIndex, InfoPaths, InfoRepodataRecord
 from .package_record import PackageRecord
 from .repodata import RepodataRecord
+from .repodata_info import RepodataInfo
 from .repodata_patch_instructions import RepodataPatchInstructions
 from .requirements_file import RequirementsTxt
 
@@ -22,6 +23,7 @@ __all__ = [
     MatchSpec,
     MetaYaml,
     PackageRecord,
+    RepodataInfo,
     RepodataRecord,
     RepodataPatchInstructions,
     RequirementsTxt,
